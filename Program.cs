731b79c2de36dbe99f301d32cf8f6fdcda9c34b1
yushulx <lingxiao1002@gmail.com>
2018-01-29 11:18:26 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using OpenCvSharp;
+using Dynamsoft.Barcode;
+using System.Runtime.InteropServices;
+
+namespace rtsp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BarcodeReader reader = new BarcodeReader("t0068MgAAALLyUZ5pborJ8XVc3efbf4XdSvDAVUonA4Z3/FiYqz1MOHaUJD3d/uBmEtXVCn9fw9WIlNw6sRT/DepkdvVW4fs=");
+            //VideoCapture capture = new VideoCapture("rtsp://184.72.239.149/vod/mp4:BigBuckBunny_175k.mov");
+            VideoCapture capture = new VideoCapture(0);
+            if (capture.IsOpened())
+            {   
+                while(true)
+                {
+                    Mat image = capture.RetrieveMat();
+                    Cv2.ImShow("video", image);
+                    int key = Cv2.WaitKey(20);
+                    // 'ESC'
+                    if (key == 27)
+                    {
+                        break;
+                    }
+
+                    // Read barcode
+                    IntPtr data = image.Data;
+                    int width = image.Width;
+                    int height = image.Height;
+                    int elemSize = image.ElemSize();
+                    int buffer_size = width * height * elemSize;
+                    byte[] buffer = new byte[buffer_size];
+                    Marshal.Copy(data, buffer, 0, buffer_size);
+                    BarcodeResult[] results = reader.DecodeBuffer(buffer, width, height, width * elemSize, ImagePixelFormat.IPF_RGB_888);
+                    if (results != null)
+                    {
+                        Console.WriteLine("Total result count: " + results.Length);
+                        foreach (BarcodeResult result in results)
+                        {
+                            Console.WriteLine(result.BarcodeText);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No barcode detected");
+                    }
+                    
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# opencv-dotnet
+# Barcode Reader with OpenCV.Net
+Use OpenCV.Net to create a barcode reader app on Windows.
+
+## Environment
+* Visual Studio 2015
+
+## How to Run
+1. Load the project to Visual Studio.
+2. Connect a webcam to PC.
+3. Build and run the app. Use 'ESC' to quit the app.
