331d1386c68cc7c6b5286d732aee034f0cdac9f6
yushulx <lingxiao1002@gmail.com>
2018-01-30 14:51:19 +0800

    Use thread

--- rtsp/Program.cs
+++ rtsp/Program.cs
@@ -9,46 +9,56 @@ namespace rtsp
     {
         static void Main(string[] args)
         {
-            BarcodeReader reader = new BarcodeReader("t0068MgAAALLyUZ5pborJ8XVc3efbf4XdSvDAVUonA4Z3/FiYqz1MOHaUJD3d/uBmEtXVCn9fw9WIlNw6sRT/DepkdvVW4fs=");
+            //BarcodeReader reader = new BarcodeReader("t0068MgAAALLyUZ5pborJ8XVc3efbf4XdSvDAVUonA4Z3/FiYqz1MOHaUJD3d/uBmEtXVCn9fw9WIlNw6sRT/DepkdvVW4fs=");
             //VideoCapture capture = new VideoCapture("rtsp://184.72.239.149/vod/mp4:BigBuckBunny_175k.mov");
-            VideoCapture capture = new VideoCapture(0);
-            if (capture.IsOpened())
-            {   
-                while(true)
-                {
-                    Mat image = capture.RetrieveMat();
-                    Cv2.ImShow("video", image);
-                    int key = Cv2.WaitKey(20);
-                    // 'ESC'
-                    if (key == 27)
-                    {
-                        break;
-                    }
-
-                    // Read barcode
-                    IntPtr data = image.Data;
-                    int width = image.Width;
-                    int height = image.Height;
-                    int elemSize = image.ElemSize();
-                    int buffer_size = width * height * elemSize;
-                    byte[] buffer = new byte[buffer_size];
-                    Marshal.Copy(data, buffer, 0, buffer_size);
-                    BarcodeResult[] results = reader.DecodeBuffer(buffer, width, height, width * elemSize, ImagePixelFormat.IPF_RGB_888);
-                    if (results != null)
-                    {
-                        Console.WriteLine("Total result count: " + results.Length);
-                        foreach (BarcodeResult result in results)
-                        {
-                            Console.WriteLine(result.BarcodeText);
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("No barcode detected");
-                    }
-                    
-                }
+            int len = 2;
+            for (int i = 0; i < len; i++)
+            {
+                StreamThread t = new StreamThread("rtsp://184.72.239.149/vod/mp4:BigBuckBunny_175k.mov", i + "");
+                t.Start();
             }
+            
+            //StreamThread t1 = new StreamThread(0, "webcam");
+            //t1.Start();
+            //VideoCapture capture = new VideoCapture(0);
+            //if (capture.IsOpened())
+            //{   
+            //    while(true)
+            //    {
+            //        Mat image = capture.RetrieveMat();
+            //        Cv2.ImShow("video", image);
+
+            //        int key = Cv2.WaitKey(20);
+            //        // 'ESC'
+            //        if (key == 27)
+            //        {
+            //            break;
+            //        }
+
+            //        // Read barcode
+            //        IntPtr data = image.Data;
+            //        int width = image.Width;
+            //        int height = image.Height;
+            //        int elemSize = image.ElemSize();
+            //        int buffer_size = width * height * elemSize;
+            //        byte[] buffer = new byte[buffer_size];
+            //        Marshal.Copy(data, buffer, 0, buffer_size);
+            //        BarcodeResult[] results = reader.DecodeBuffer(buffer, width, height, width * elemSize, ImagePixelFormat.IPF_RGB_888);
+            //        if (results != null)
+            //        {
+            //            Console.WriteLine("Total result count: " + results.Length);
+            //            foreach (BarcodeResult result in results)
+            //            {
+            //                Console.WriteLine(result.BarcodeText);
+            //            }
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("No barcode detected");
+            //        }
+
+            //    }
+            //}
         }
     }
 }
